20d0d03d14e191cdd93a2937e7aca594e63b9581
Jefferson Rylee <rylee.jeff385@gmail.com>
2019-07-06 10:38:58 +0800

    Add SplashActivity Add webview Add logo Add colors Add styles Add strings

--- app/src/main/java/com/arnesfield/askify/MainActivity.kt
+++ app/src/main/java/com/arnesfield/askify/MainActivity.kt
@@ -2,11 +2,74 @@ package com.arnesfield.askify
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.webkit.WebChromeClient
+import android.annotation.SuppressLint
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    private val DEV = false
+    private val TAG = "tagx"
+    private val URL = if (DEV) "http://" else "http://askify.x10.mx/"
+
+    private lateinit var webView: WebView
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+    private lateinit var refreshBlock: View
+    private var flag: Int = 0
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.webView = findViewById(R.id.webview)
+        this.textView = findViewById(R.id.textView)
+        this.button = findViewById(R.id.button)
+        this.refreshBlock = findViewById(R.id.refreshBlock)
+
+        flag = View.GONE
+
+        button.setOnClickListener {
+            load()
+        }
+
+        webView.settings.javaScriptEnabled = true
+
+        val activity = this
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView, progress: Int) {
+                // Activities and WebViews measure progress with different scales.
+                // The progress meter will automatically disappear when we reach 100%
+                activity.setProgress(progress * 1000)
+            }
+        }
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                Log.d(TAG, "2")
+                refreshBlock.visibility = if (flag == View.GONE) View.VISIBLE else flag
+                webView.visibility = flag
+            }
+
+            override fun onReceivedError(view: WebView, errorCode: Int, description: String, failingUrl: String) {
+                flag = View.GONE
+                Log.d(TAG, "1")
+                Toast.makeText(activity, "Oh no! $description", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        this.load()
     }
+
+    private fun load() {
+        flag = View.VISIBLE
+        webView.loadUrl(URL)
+    }
+
 }
